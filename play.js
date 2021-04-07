2cd237e9dd55f930af5f2396a5a808936b79b62e
Grigor Baltadjian <gb795@hotmail.com>
2021-04-07 18:24:16 +0000

    playFinal

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', function(data) {
-    console.log('Received: ' + data);
-  });
-  return conn;
-}
-
-
-
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+
+
+setupInput(connect());
